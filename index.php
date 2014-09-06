bc27b79dfbd2dd4719e79d9a89257ffa8a85aada
Tom English <redsoxfantom@gmail.com>
2014-09-06 01:23:34 -0400

    added ability to scramble selected word

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 <?php
         $contents = file("data/dict");
         $line = $contents[rand(0,count($contents)-1)];
-        echo "Your word is: $line\n"
+	$line = trim($line);
+        echo "Your word is: $line</br>";
+	$scrambled_line = str_shuffle($line);
+	echo "Your jumbled word is: $scrambled_line</br>";
 ?>
 
  </body>
