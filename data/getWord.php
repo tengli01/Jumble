b9c58f2dd393d6e995d8ba47a84234222103c0dd
Tom <tengli01@villanova.edu>
2014-10-31 16:42:20 -0400

    Fixed word selection algorithm

--- data/getWord.php
+++ data/getWord.php
@@ -1,12 +1,10 @@
 <?php
-// This function gets a new word list from the dictionary and sends it to the client. It is a horrible O(n^2) algorithm and needs refactoring
-// One possibility is to make a single pass through the dictionary, collecting words that match any letter in the special word
+// This function gets a new word list from the dictionary and sends it to the client.
 // The result is stored in the 'Data_div'
 // EX: bat,0,ball,2,drat,1,at
 //	Special word is 'bat', the first word to guess is 'ball' and the index of the letter making up part of the special word is '0'
 
-$contents = file(__DIR__."/dict");
-$special_contents = file(__DIR__."/special_dict");
+$special_contents = file(__DIR__."/dictionaries/special_dict");
 $special_line = trim($special_contents[rand(0,count($special_contents)-1)]);
 $special_line_scrambled = str_shuffle($special_line);
 $special_line_array = str_split($special_line_scrambled);
@@ -17,26 +15,14 @@ echo($special_line . ",");
 //For each character in the $special_line
 for($count=0; $count<count($special_line_array); $count++)
 {
-	$possible_words = array();
-	$possible_letter_locations = array();
-	//For each word in the original dictionary
-	for($idx=0; $idx<count($contents); $idx++)
-	{
-		//Check if the letter from the special word appears in the word
-		$position = strpos($contents[$idx],$special_line_array[$count]);
-		
-		//If a particular word contains the letter from the $special_line
-		if($position != False)
-		{
-			array_push($possible_words,trim($contents[$idx]));
-			array_push($possible_letter_locations,$position);
-		}
-	}
+	//Read the file named the same as the letter from the special word
+	$letter_from_special_word = $special_line_array[$count];
+	$possible_words = file(__DIR__."/dictionaries/".$letter_from_special_word);
 	
 	//Now that we have an array of words, pick a random one
 	$chosen_index = rand(0,count($possible_words)-1);
-	$chosen_word = $possible_words[$chosen_index];
-	$chosen_letter_locations = $possible_letter_locations[$chosen_index];
+	$chosen_word = trim($possible_words[$chosen_index]);
+	$chosen_letter_locations = strpos($chosen_word,$letter_from_special_word);
 	echo($chosen_letter_locations.",".$chosen_word);
 	
 	//The last word in the array should not have a comma after it
